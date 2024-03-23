2a685cec1e0bcb570bfdbfc6448dab78e41dac39
Alejandro Moreo <alejandro.moreo@isti.cnr.it>
2024-03-23 20:12:10 +0100

    seems to be working :D

--- Retrieval/commons.py
+++ Retrieval/commons.py
@@ -5,6 +5,7 @@ from os.path import join
 
 from quapy.data import LabelledCollection
 from quapy.protocol import AbstractProtocol
+import json
 
 
 def load_txt_sample(path, parse_columns, verbose=False, max_lines=None):
@@ -40,24 +41,70 @@ def load_txt_sample(path, parse_columns, verbose=False, max_lines=None):
     return X, y
 
 
+def load_json_sample(path, class_name, max_lines=-1):
+    obj = json.load(open(path, 'rt'))
+    keys = [f'{id}' for id in range(len(obj['text'].keys()))]
+    text = [obj['text'][id] for id in keys]
+    classes = [obj[class_name][id] for id in keys]
+    if max_lines is not None and max_lines>0:
+        text = text[:max_lines]
+        classes = classes[:max_lines]
+    return text, classes
+
+
+class TextRankings:
+
+    def __init__(self, path, class_name):
+        self.obj = json.load(open(path, 'rt'))
+        self.class_name = class_name
+
+    def get_sample_Xy(self, sample_id, max_lines=-1):
+        sample_id = str(sample_id)
+        O = self.obj
+        docs_ids = [doc_id for doc_id, query_id in O['qid'].items() if query_id == sample_id]
+        texts = [O['text'][doc_id] for doc_id in docs_ids]
+        labels = [O['continent'][doc_id] for doc_id in docs_ids]
+        if max_lines > 0 and len(texts) > max_lines:
+            ranks = [int(O['rank'][doc_id]) for doc_id in docs_ids]
+            sel = np.argsort(ranks)[:max_lines]
+            texts = np.asarray(texts)[sel]
+            labels = np.asarray(labels)[sel]
+
+        return texts, labels
+
+
+def get_query_id_from_path(path, prefix='training', posfix='200SPLIT'):
+    qid = path
+    qid = qid[:qid.index(posfix)]
+    qid = qid[qid.index(prefix)+len(prefix):]
+    return qid
+
+
 class RetrievedSamples(AbstractProtocol):
 
-    def __init__(self, path_dir: str, load_fn, vectorizer, max_train_lines=None, max_test_lines=None, classes=None):
+    def __init__(self, path_dir: str, load_fn, vectorizer, max_train_lines=None, max_test_lines=None, classes=None, class_name=None):
         self.path_dir = path_dir
         self.load_fn = load_fn
         self.vectorizer = vectorizer
         self.max_train_lines = max_train_lines
         self.max_test_lines = max_test_lines
         self.classes=classes
+        assert class_name is not None, 'class name should be specified'
+        self.class_name = class_name
+        self.text_samples = TextRankings(join(self.path_dir, 'testRankingsRetrieval.json'), class_name=class_name)
+
 
     def __call__(self):
-        for file in glob(join(self.path_dir, 'test_rankings', 'test_rankingstraining_rankings_*.txt')):
 
-            X, y = self.load_fn(file.replace('test_', 'training_'), parse_columns=True, max_lines=self.max_train_lines)
+        for file in glob(join(self.path_dir, 'training*SPLIT.json')):
+
+            X, y = self.load_fn(file, class_name=self.class_name, max_lines=self.max_train_lines)
             X = self.vectorizer.transform(X)
             train_sample = LabelledCollection(X, y, classes=self.classes)
 
-            X, y = self.load_fn(file, parse_columns=True, max_lines=self.max_test_lines)
+            query_id = get_query_id_from_path(file)
+            X, y = self.text_samples.get_sample_Xy(query_id, max_lines=self.max_test_lines)
+
             # if len(X)!=qp.environ['SAMPLE_SIZE']:
             #     print(f'[warning]: file {file} contains {len(X)} instances (expected: {qp.environ["SAMPLE_SIZE"]})')
             # assert len(X) == qp.environ['SAMPLE_SIZE'], f'unexpected sample size for file {file}, found {len(X)}'
--- Retrieval/fifth.py
+++ Retrieval/fifth.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -7,7 +9,8 @@ from sklearn.svm import LinearSVC
 
 import quapy as qp
 import quapy.functional as F
-from Retrieval.commons import RetrievedSamples, load_txt_sample
+from Retrieval.commons import RetrievedSamples, load_txt_sample, load_json_sample
+from Retrieval.tabular import Table
 from method.non_aggregative import MaximumLikelihoodPrevalenceEstimation
 from quapy.method.aggregative import ClassifyAndCount, EMQ, ACC, PCC, PACC, KDEyML
 from quapy.protocol import AbstractProtocol
@@ -27,6 +30,7 @@ Por ahora 1000 en tr y 100 en test
 Parece que ahora hay muy poco shift  
 """
 
+
 def cls(classifier_trained=None):
     if classifier_trained is None:
         # return LinearSVC()
@@ -37,31 +41,31 @@ def cls(classifier_trained=None):
 
 def methods(classifier_trained=None):
     yield ('CC', ClassifyAndCount(cls(classifier_trained)))
+    yield ('PCC', PCC(cls(classifier_trained)))
+    yield ('ACC', ACC(cls(classifier_trained), val_split=5, n_jobs=-1))
     yield ('PACC', PACC(cls(classifier_trained), val_split=5, n_jobs=-1))
     yield ('EMQ', EMQ(cls(classifier_trained), exact_train_prev=True))
     yield ('EMQh', EMQ(cls(classifier_trained), exact_train_prev=False))
-    yield ('EMQ-BCTS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='bcts'))
-    yield ('EMQ-TS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='ts'))
-    yield ('EMQ-NBVS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='nbvs'))
+    # yield ('EMQ-BCTS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='bcts'))
+    # yield ('EMQ-TS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='ts'))
+    # yield ('EMQ-NBVS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='nbvs'))
     # yield ('EMQ-VS', EMQ(cls(classifier_trained), exact_train_prev=False, recalib='vs'))
-    yield ('PCC', PCC(cls(classifier_trained)))
-    yield ('ACC', ACC(cls(classifier_trained), val_split=5, n_jobs=-1))
-    yield ('KDE001', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.001))
-    yield ('KDE005', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.005)) # <-- wow!
-    yield ('KDE01', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.01))
-    yield ('KDE02', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.02))
-    yield ('KDE03', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.03))
-    yield ('KDE05', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.05))
+    # yield ('KDE001', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.001))
+    # yield ('KDE005', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.005)) # <-- wow!
+    # yield ('KDE01', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.01))
+    # yield ('KDE02', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.02))
+    # yield ('KDE03', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.03))
+    # yield ('KDE05', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.05))
     yield ('KDE07', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.07))
-    yield ('KDE10', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.10))
+    # yield ('KDE10', KDEyML(cls(classifier_trained), val_split=5, n_jobs=-1, bandwidth=0.10))
     yield ('MLPE', MaximumLikelihoodPrevalenceEstimation())
 
 
 def train_classifier():
     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=10)
-    training = LabelledCollection.load(train_path, loader_func=load_txt_sample, verbose=True, parse_columns=False)
+    training = LabelledCollection.load(train_path, loader_func=load_json_sample, class_name=CLASS_NAME)
 
-    if REDUCE_TR > 0:
+    if REDUCE_TR > 0 and len(training) > REDUCE_TR:
         print('Reducing the number of documents in the training to', REDUCE_TR)
         training = training.sampling(REDUCE_TR, *training.prevalence())
 
@@ -89,69 +93,77 @@ def train_classifier():
     return tfidf, classifier_trained
 
 
+def reduceAtK(data: LabelledCollection, k):
+    X, y = data.Xy
+    X = X[:k]
+    y = y[:k]
+    return LabelledCollection(X, y, classes=data.classes_)
 
-RANK_AT_K = 1000
+
+RANK_AT_K = -1
 REDUCE_TR = 50000
 qp.environ['SAMPLE_SIZE'] = RANK_AT_K
 
-data_path = './50_50_split_trec'
-train_path = join(data_path, 'train_50_50_continent.txt')
+data_path = './newExperimentalSetup'
+train_path = join(data_path, 'train3000samples.json')
 
-tfidf, classifier_trained = qp.util.pickled_resource('classifier.pkl', train_classifier)
-trained=True
 
-experiment_prot = RetrievedSamples(data_path,
-                           load_fn=load_txt_sample,
-                           vectorizer=tfidf,
-                           max_train_lines=None,
-                           max_test_lines=RANK_AT_K, classes=classifier_trained.classes_)
+Ks = [10, 50, 100, 250, 500, 1000, 2000]
+# Ks = [500]
 
-result_mae_dict = {}
-result_mrae_dict = {}
-for method_name, quantifier in methods(classifier_trained):
-    # print('Starting with method=', method_name)
+for CLASS_NAME in ['continent']: #, 'gender', 'gender_category', 'occupations', 'source_countries', 'source_subcont_regions', 'years_category', 'relative_pageviews_category']:
 
-    mae_errors = []
-    mrae_errors = []
-    pbar = tqdm(experiment_prot(), total=49)
-    for train, test in pbar:
-        if train is not None:
-            try:
+    tfidf, classifier_trained = qp.util.pickled_resource(f'classifier_{CLASS_NAME}.pkl', train_classifier)
+    trained=True
 
-                # print(train.prevalence())
-                # print(test.prevalence())
-                if trained and method_name!='MLPE':
-                    quantifier.fit(train, val_split=train, fit_classifier=False)
-                else:
-                    quantifier.fit(train)
-                estim_prev = quantifier.quantify(test.instances)
+    experiment_prot = RetrievedSamples(data_path,
+                               load_fn=load_json_sample,
+                               vectorizer=tfidf,
+                               max_train_lines=None,
+                               max_test_lines=RANK_AT_K, classes=classifier_trained.classes_, class_name=CLASS_NAME)
 
-                mae = qp.error.mae(test.prevalence(), estim_prev)
-                mae_errors.append(mae)
+    method_names = [name for name, *other in methods()]
+    benchmarks = [f'{CLASS_NAME}@{k}' for k in Ks]
+    table_mae = Table(benchmarks, method_names, color_mode='global')
+    table_mrae = Table(benchmarks, method_names, color_mode='global')
 
-                mrae = qp.error.mrae(test.prevalence(), estim_prev)
-                mrae_errors.append(mrae)
+    for method_name, quantifier in methods(classifier_trained):
+        # print('Starting with method=', method_name)
 
-                # print()
-                # print('Training prevalence:', F.strprev(train.prevalence()), 'shape', train.X.shape)
-                # print('Test prevalence:', F.strprev(test.prevalence()), 'shape', test.X.shape)
-                # print('Estim prevalence:', F.strprev(estim_prev))
+        mae_errors = {k:[] for k in Ks}
+        mrae_errors = {k:[] for k in Ks}
 
-            except Exception as e:
-                print(f'wow, something happened here! skipping; {e}')
-        else:
-            print('skipping one!')
+        pbar = tqdm(experiment_prot(), total=49)
+        for train, test in pbar:
+            if train is not None:
+                try:
+                    if trained and method_name!='MLPE':
+                        quantifier.fit(train, val_split=train, fit_classifier=False)
+                    else:
+                        quantifier.fit(train)
 
-        pbar.set_description(f'{method_name}\tmae={np.mean(mae_errors):.4f}\tmrae={np.mean(mrae_errors):.4f}')
-    print()
-    result_mae_dict[method_name] = np.mean(mae_errors)
-    result_mrae_dict[method_name] = np.mean(mrae_errors)
+                    for k in Ks:
+                        test_k = reduceAtK(test, k)
+                        estim_prev = quantifier.quantify(test_k.instances)
 
-print('Results\n'+('-'*100))
-for method_name in result_mae_dict.keys():
-    MAE = result_mae_dict[method_name]
-    MRAE = result_mrae_dict[method_name]
-    print(f'{method_name}\t{MAE=:.5f}\t{MRAE=:.5f}')
+                        mae_errors[k].append(qp.error.mae(test_k.prevalence(), estim_prev))
+                        mrae_errors[k].append(qp.error.mrae(test_k.prevalence(), estim_prev, eps=(1./(2*k))))
+
+                except Exception as e:
+                    print(f'wow, something happened here! skipping; {e}')
+            else:
+                print('skipping one!')
+
+            # pbar.set_description(f'{method_name}\tmae={np.mean(mae_errors):.4f}\tmrae={np.mean(mrae_errors):.4f}')
+            pbar.set_description(f'{method_name}')
+
+        for k in Ks:
+            table_mae.add(benchmark=f'{CLASS_NAME}@{k}', method=method_name, values=mae_errors[k])
+            table_mrae.add(benchmark=f'{CLASS_NAME}@{k}', method=method_name, values=mrae_errors[k])
+
+    table_mae.latexPDF('./latex', 'table_mae.tex')
+    table_mrae.latexPDF('./latex', 'table_mrae.tex')
+
 
 
 
